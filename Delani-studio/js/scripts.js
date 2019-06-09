a077fcfd25e6111476fd619dd81c98b3dcdd326f
Michelle Mukami <you@example.com>
2019-06-09 18:01:30 +0300

    changes made in js

--- Delani-studio/js/scripts.js
+++ Delani-studio/js/scripts.js
@@ -2,8 +2,13 @@ $('.main').click(function() {
   $(this).children('.hide').slideToggle(1200);
   $(this).children('.show').toggle(1500);
 });
-$("button").click(function(kami) {
-  var client = document.getElementById('nameDetail').value;
-  alert(' Dear ' + client + ' Message is well recieved ' + ' you may contact us at will');
+$("button").click(function (kami) {
+  var client = document.getElementById('client').value;
+  alert( client +' we have received your message');
   kami.preventDefault();
+});
+$("button").on('click', function() {
+  $('form').each(function() {
+    this.reset();
+  });
 });
